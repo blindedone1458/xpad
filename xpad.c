cc0cec32bb45414dee0fa5920b6a75d27b500593
blindedone1458 <mman002@neuric.net>
2025-03-15 01:18:53 -0500

    found and fixed compare issue

--- xpad.c
+++ xpad.c
@@ -2386,16 +2386,16 @@ static int xpad_probe(struct usb_interface *intf, const struct usb_device_id *id
 	xpad->intf = intf;
 	xpad->mapping = xpad_device[i].mapping;
 
-	pr_warn("%s:: checking for xpad flavor of udev->product: %s\n", KBUILD_MODNAME, udev->product);
 	if (udev->product) {
-		
 		for(j = 0; xpad_flavor[j].idVendor; j++) {
+			if ((xpad_device[i].idVendor == xpad_flavor[j].idVendor) && 
+					(xpad_device[i].idProduct == xpad_flavor[j].idProduct)) {
 				if (!strcmp(udev->product, xpad_flavor[j].product)) {
 					pr_warn("%s:: loaded xpad flavor: %s\n", KBUILD_MODNAME, xpad_flavor[j].product);
 					xpad->mapping |= xpad_flavor[j].mapping;
 					break;
 				}
-			
+			}
 		}
 	}
 
